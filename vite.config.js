7a8498eca85a486fa675177630addaf0d5416f3e
root <root@srv1466371.hstgr.cloud>
2026-03-07 18:16:00 +0000

    build: otimiza bundle com split de vendor e compressão gzip

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
+        viteCompression({ algorithm: 'gzip', ext: '.gz' }),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom'],
+                },
+            },
+        },
+        cssCodeSplit: true,
+        minify: 'terser',
+        terserOptions: {
+            compress: { drop_console: true },
+        },
+    },
 })
